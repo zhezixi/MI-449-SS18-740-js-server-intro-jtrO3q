af63e3f143c0970c2fd39709461af3e1e99fe5b3
Sihe Zhang <liubing1@msu.edu>
2018-04-19 01:39:35 -0400

    small changes applied

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ var http = require('http')
 // Create a new server that just says "Hi!!" at every route
 var server = http.createServer(function (request, response) {
   if (request.url === '/') {
-    response.end(
-      '<h1>Welcome!</h1>'
-    )
-  } else if (request.url === '/randomjoke') {
-      var randomNumber = Math.floor(Math.random() * 2)) + 1
+    response.end('<h1>Welcome!</h1>')
+  } else if (request.url === '/random-joke') {
+      var randomNumber = Math.floor(Math.random() * 2) + 1
       if (randomNumber = 1) {
         response.write('<p>Knock, knock!<br>Who’s there?<br>Opportunity!<br>That is impossible. Opportunity doesn’t come knocking twice!</p>')
       } else if (randomNumber = 2) {
@@ -16,14 +14,13 @@ var server = http.createServer(function (request, response) {
       } else {
         response.write('<p>Knock knock.<br>Who’s there?<br>Beats.<br>Beats who?<br>Beats me.</p>')
       }
-  }
-    response.end('<p>Homepage<a href="/">click here</a>!</p>')
+      response.end('<p><a href="/">Homepage</a></p>')
   } else if (request.url === '/cuteness') {
-    response.write('<h1>Everyone likes cute kitten!</h1><img src="https://i.pinimg.com/originals/00/a2/5e/00a25e20bc5b615799588d377bfaac5e.jpg"')
-    response.end('<p>Homepage<a href="/">click here</a>!</p>')
+    response.write('<h1>Everyone likes cute kitten!</h1><img src="https://i.pinimg.com/originals/00/a2/5e/00a25e20bc5b615799588d377bfaac5e.jpg">')
+    response.end('<p><a href="/">Homepage</a></p>')
   } else {
     response.write('<h1>404. That is an ERROR!</h1><br><p>The requested URL' + request.url + ' was not found on this server. That is all we know.</p>')
-    response.end('<p>Homepage<a href="/">click here</a>!</p>')
+    response.end('<p><a href="/">Homepage</a></p>')
   }
 })
 
